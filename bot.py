48e2c85d33220fc37cf7ca8d4b20a90e06307f66
satishkrmeena8066 <satishkrmeena8066@gmail.com>
2026-07-07 03:51:42 +0530

    Implement chat join request approval bot

---
+++ bot.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
+
+BOT_TOKEN = "8360228300: AAGWZj GV4v6JTeYcz72zFuxGOC
+st6VBVlpQ"
+CHANNEL_ID = -1004485039141
+
+async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        await context.bot.approve_chat_join_request(
+            chat_id=CHANNEL_ID,
+            user_id=update.chat_join_request.from_user.id
+        )
+        print(f"Approved: {update.chat_join_request.from_user.id}")
+    except Exception as e:
+        print(e)
+
+app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(ChatJoinRequestHandler(approve))
+app.run_polling()
