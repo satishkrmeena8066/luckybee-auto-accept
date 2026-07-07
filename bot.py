7185992134cf880d7b969d834182bb2f4843369b
satishkrmeena8066 <satishkrmeena8066@gmail.com>
2026-07-08 00:21:31 +0530

    Refactor bot.py to enhance user handling and messages

--- bot.py
+++ bot.py
@@ -1,46 +1,79 @@
+import sqlite3
 from telegram import Update
-from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    ChatJoinRequestHandler,
+    CommandHandler,
+    ContextTypes,
+)
 
 BOT_TOKEN = "8360228300:AAG43M1a7Hu2JqDz-i_Qffnum-V61h1AUfw"
 CHANNEL_ID = -1004485039141
+ADMIN_ID = 7324304740
 
+db = sqlite3.connect("users.db", check_same_thread=False)
+cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    first_name TEXT
+)
+""")
+
+db.commit() 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    user = update.effective_user
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO users(user_id,first_name) VALUES(?,?)",
+        (user.id, user.first_name),
+    )
+
+    db.commit()
+
+    await update.message.reply_text(
+        f"👋 Welcome {user.first_name}\n\n"
+        "🎉 Welcome to LuckyBee Team.\n\n"
+        "You'll receive all offers here."
+    )
 async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        # Approve Join Request
+  try:
         await update.chat_join_request.approve()
 
         user = update.chat_join_request.from_user
-        user_id = user.id
-        first_name = user.first_name
 
-        print(f"Approved: {user_id}")
+        cursor.execute(
+            "INSERT OR IGNORE INTO users(user_id, first_name) VALUES(?, ?)",
+            (user.id, user.first_name),
+        )
+        db.commit()
 
-        # Send Welcome Message
         try:
             await context.bot.send_message(
-                chat_id=user_id,
-                text=f"""🎉 Welcome {first_name}!
+                chat_id=user.id,
+                text=f"""🎉 Welcome {user.first_name}!
 
-✅ Your join request has been approved successfully.
+✅ Your request has been approved.
 
-💚 Thank you for joining LuckyBee Team.
+💚 Welcome to LuckyBee Team.
 
-Stay connected for daily updates and exciting offers!"""
+Stay connected for daily updates, offers and rewards."""
             )
-            print(f"Welcome message sent to {user_id}")
-
         except Exception as e:
             print(f"DM Failed: {e}")
 
+        print(f"Approved: {user.id}")
+
     except Exception as e:
-        print(f"Approve Failed: {e}")
-
-app = Application.builder().token(BOT_TOKEN).build()
-
+        print(e)
+        app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 app.add_handler(ChatJoinRequestHandler(approve))
 
 print("LuckyBee Bot Started...")
 
 app.run_polling(
-    allowed_updates=["chat_join_request"]
+    allowed_updates=["message", "chat_join_request"]
 )
